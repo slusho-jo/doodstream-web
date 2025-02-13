e5d1c2e03768519f23ea6c01a5370ccd92ea9e76
slusho <chinmagus@gmail.com>
2025-02-14 03:51:48 +0700

    update

--- app/template.tsx
+++ app/template.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 export default function Template({ children }: { children: ReactNode }) {
     return (
         <main className="min-h-screen bg-gradient-to-b from-background to-background/95">
-            <Navbar />
+            <Navbar folders={[]} />
             <div className="container mx-auto px-4 py-6 md:px-6 lg:px-8 max-w-7xl">
                 {children}
             </div>
